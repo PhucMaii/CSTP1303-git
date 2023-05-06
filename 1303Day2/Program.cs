b87f800e8df37b99a0999c1a1f99001309d27c3c
Thien Phuc Mai <000461754@student.vcc.ca>
2023-05-06 11:18:20 -0700

    add testReadLine

---
+++ 1303Day2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _1303Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Enter one or more lines - press CTRL+Z to exit):");
+            string line;
+
+            do
+            {
+                Console.Write(" ==> ");
+                line = Console.ReadLine();
+                if (line != null)
+                {
+                    Console.WriteLine($" {line}");
+                }
+            } while (line != null);
+        }
+    }
+}
